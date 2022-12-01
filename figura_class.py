b452553fbbdca91da62681cb002513e2f0168e63
javsmar <mastos44@gmail.com>
2022-12-01 22:28:45 +0100

    tercer commit pong

--- figura_class.py
+++ figura_class.py
@@ -1,20 +1,30 @@
 import pygame as pg
 
 class Pelota():
-    def __init__(self,pos_x,pos_y,radio=10,color=(255,255,255),vx=1,vy=1):
+    def __init__(self,pos_x,pos_y,radio=10,color=(231,23,238),vx=1,vy=1):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.radio = radio
         self.color = color
         self.vx = vx
         self.vy = vy
+        
+    def mover(self,y_max = 600, x_max = 800):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
+        
+        if self.pos_x >= x_max-self.radio or self.pos_x < 0 + self.radio:
+            self.vx *= -1
+        if self.pos_y >= y_max-self.radio or self.pos_y <= 0 + self.radio:
+            self.vy *= -1
+        
     
     def dibujar(self, pantalla):
         pg.draw.circle(pantalla,self.color,(self.pos_x,self.pos_y),self.radio)
         
 
 class Raqueta():
-    def __init__(self,pos_x,pos_y,w = 20, h= 20, color=(255,255,255),vx=1,vy=1):
+    def __init__(self,pos_x, pos_y, w = 20, h= 100, color=(255,255,255),vx=1,vy=1):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.w = w
@@ -22,6 +32,14 @@ class Raqueta():
         self.color = color
         self.vx = vx
         self.vy = vy
+    
+    def mover(self,tecla_arriba,tecla_abajo,y_max = 600, y_min = 0):
+        estado_teclas = pg.key.get_pressed()
+        # Mover las teclas de arriba abajo --- para poner limite a la raqueta y no se salga de la pantalla
+        if estado_teclas[tecla_arriba] == True and self.pos_y > y_min + self.h//2: 
+            self.pos_y -= self.vy
+        elif estado_teclas[tecla_abajo] == True and self.pos_y < y_max - self.h//2:
+            self.pos_y += self.vy
         
     def dibujar(self,pantalla):
           pg.draw.rect(pantalla,self.color,(self.pos_x-(self.w//2),self.pos_y-(self.h//2),self.w,self.h))
--- main.py
+++ main.py
@@ -4,3 +4,35 @@ import pygame as pg
 pg.init()
 
 pantalla_principal = pg.display.set_mode((800,600))
+pg.display.set_caption("PONG")
+
+cronometro = pg.time.Clock()#Tasa de refresco de los fotogramas por segundo de nuestro bucle
+
+game_over = False
+
+pelota = Pelota(400,300)
+
+raqueta1 = Raqueta(10,300,color=(191,20,12),vx=2,vy=2)
+raqueta2 = Raqueta(790,300,color=(22,238,234),vx=2,vy=2)
+
+while not game_over:
+    vt = cronometro.tick(800)#Tasa de refresco de los fotogramas por segundo de nuestro bucle
+    
+    for eventos in pg.event.get():
+        print(eventos)
+        if eventos.type == pg.QUIT:
+            game_over = True
+           
+    pantalla_principal.fill((8, 130, 28))
+    
+    pg.draw.line(pantalla_principal, (255,255,255),(400,0),(400,600),width=5) 
+    
+    pelota.dibujar(pantalla_principal)
+    pelota.mover()
+    raqueta1.dibujar(pantalla_principal)
+    raqueta1.mover(pg.K_w,pg.K_s)
+    raqueta2.dibujar(pantalla_principal)
+    raqueta2.mover(pg.K_UP,pg.K_DOWN)
+    
+    pg.display.flip()
+pg.quit
